2594903c31fcefff8ef2de3f843a21fa7b4f83a5
dai_48k <1981669259@qq.com>
2025-09-01 09:40:24 +0800

    快捷粘贴板一键粘贴添加任务

--- src/main/java/top/dreamcenter/spider/App.java
+++ src/main/java/top/dreamcenter/spider/App.java
@@ -9,6 +9,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.*;
@@ -57,6 +62,9 @@ public class App {
         frame.setLayout(new BorderLayout());
         frame.setSize(400, 300);
 
+        JPanel southPanel = new JPanel();
+        southPanel.setLayout(new BorderLayout());
+
         JTextField input = new JTextField();
         queue = new LinkedList<>();
         input.addKeyListener(new KeyAdapter() {
@@ -65,17 +73,20 @@ public class App {
                 if (e.getKeyChar() == '\n') {
                     String articleLink = input.getText();
                     input.setText("");
-                    queue.add(articleLink);
-                    append("新入队列，队列排队中: " + queue.size());
-
-                    // 有新加入的时候，如果线程正在跑，则不管，否则新建线程获取数据
-                    if (thread == null || !thread.isAlive()) {
-                        task();
-                    }
+                    joinAndRun(articleLink);
                 }
             }
         });
 
+        JButton button = new JButton("智能粘贴");
+        button.setBackground(Color.white);
+        button.addActionListener(e -> {
+            String clipboardData = getClipboardData();
+            joinAndRun(clipboardData);
+        });
+
+        southPanel.add(input, BorderLayout.CENTER);
+        southPanel.add(button, BorderLayout.EAST);
 
 
         textArea = new JTextArea();
@@ -89,9 +100,43 @@ public class App {
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         frame.add(scrollPane, BorderLayout.CENTER);
-        frame.add(input, BorderLayout.SOUTH);
+        frame.add(southPanel, BorderLayout.SOUTH);
+
+        input.requestFocus();
+
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private static void joinAndRun(String url) {
+        queue.add(url);
+        append("⇨ 新入队列，队列排队中: " + queue.size());
+
+        // 有新加入的时候，如果线程正在跑，则不管，否则新建线程获取数据
+        if (thread == null || !thread.isAlive()) {
+            task();
+        }
+    }
+
+    private static String getClipboardData() {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+        try {
+            // 检查剪贴板是否包含字符串类型的数据
+            if (clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
+                // 获取剪贴板中的文本数据
+                String result = (String) clipboard.getData(DataFlavor.stringFlavor);
+                System.out.println("剪贴板中的文本内容是：" + result);
+                return result;
+            } else {
+                System.out.println("剪贴板中没有文本数据。");
+                return null;
+            }
+        } catch (UnsupportedFlavorException | IOException e) {
+            System.err.println("无法访问剪贴板中的数据：" + e.getMessage());
+            return null;
+        }
+
     }
 
     static void task() {
@@ -99,13 +144,13 @@ public class App {
             while (!queue.isEmpty()) {
                 String link = queue.poll();
                 if (link != null) {
-                    append("开始任务:" + link);
+                    append("▼ 开始任务:" + link);
                     try {
                         startOneDownload(link);
                     } catch (Exception ex) {
-                        append("[EXCEPTION] " + ex.getMessage());
+                        append("▷ [EXCEPTION] " + ex.getMessage());
                     }
-                    append("队列剩余: " + queue.size() + "\n");
+                    append("▲ 队列剩余: " + queue.size() + "\n");
                 }
             }
         });
@@ -117,7 +162,7 @@ public class App {
     }
 
     private static void startOneDownload(String articleLink) {
-        append("导航至:" + articleLink);
+        append("▷ 导航至:" + articleLink);
 
         // 无头模式要设置user-agent，否则会被识别到
         ChromeOptions options=new ChromeOptions();
@@ -160,7 +205,7 @@ public class App {
                 links.add(result);
             }
         }
-        append("找到图片:" + total +" 张");
+        append("▷ 找到图片:" + total +" 张");
 
         driver.quit();
 
@@ -173,7 +218,7 @@ public class App {
         }
 
 
-        append("成功:" + success + " / " + total + "");
+        append("▷ 成功:" + success + " / " + total + "");
     }
 
     private static boolean download(String basePath, String fileName, String link) {
